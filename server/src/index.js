7fea90f756cb095a93263d245985d9d912cb9c36
Perez Cato <perezcatoc@gmail.com>
2020-04-19 08:00:37 +0000

    added an error handler

--- server/src/index.js
+++ server/src/index.js
@@ -13,6 +13,22 @@ app.use(cors({
   origin: 'http://localhost:3000',
 }));
 
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(400);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  const statusCode = req.statusCode === 200 ? 500 : req.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: error.stack,
+  });
+});
+
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
   debug('Application is running');
