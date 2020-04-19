98e3910bf21f27aa06b5155e69b6a71dc668e25e
Perez Cato <perezcatoc@gmail.com>
2020-04-19 19:01:45 +0000

    added the mongoose to the project and refactored the index file

---
+++ server/src/config/app.config.js
@@ -0,0 +1,32 @@
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import { debuglog } from 'util';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const appConfig = (app) => {
+  const debug = debuglog(process.env.APP_DEBUG);
+  mongoose.connect(process.env.DB_CONNECTION,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  const db = mongoose.connection;
+
+  db.on('error', () => {
+    debug('Something went wrong');
+  });
+  db.on('success', () => {
+    debug('Database Connected');
+  });
+  app.use(morgan('combined'));
+  app.use(helmet());
+  app.use(cors({
+    origin: 'http://localhost:3000',
+  }));
+};
+
+export default appConfig;
